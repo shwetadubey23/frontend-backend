a3a12c5da59c1d3abe7c1855f3272452be8b61f1
shwetadubey23 <shwetadubey0246@gmail.com>
2023-05-16 22:27:34 +0530

    adding-a-register-page

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const path = require('path')
 const mongoose = require('mongoose')
 const cookieParser = require('cookie-parser')
 const jwt = require('jsonwebtoken')
+const bcrypt = require('bcrypt')
+
 
 const app = express();
 
@@ -28,52 +30,84 @@ mongoose.connect("mongodb+srv://Shwetadubey:QvtqJ8hdhmn0fhlT@cluster0.ymyddly.mo
 const userSchema = new mongoose.Schema({
   name: String,
   email: String,
+  password: String,
 
 })
 const userInfo = mongoose.model("User", userSchema)
 
-const isAuthenticated = async (req, res,next) => {
-  const {token} = req.cookies
-  if(token) {
+const isAuthenticated = async (req, res, next) => {
+  const { token } = req.cookies
+  if (token) {
     const decodedToken = jwt.verify(token, "wenhuebfrunsd")
-req.user = await userInfo.findById(decodedToken._id)
+    req.user = await userInfo.findById(decodedToken._id)
     next()
   } else {
-    res.render("login")
+    res.redirect("/login")
   }
 }
 
 
 app.get('/Home', isAuthenticated, (req, res) => {
-  console.log(req.user);
-  res.render("logout", {name: req.user.name})
+  res.render("logout", { name: req.user.name })
 });
-  // res.sendFile("index.html")
+// res.sendFile("index.html")
+
+app.get('/login', (req, res) => {
+  res.render("login");
+})
+
+app.get("/register", (req, res) => {
+  res.render("register");
+})
+
+app.post('/register', async (req, res) => {
+  // console.log(req.user);
+  const { name, email, password } = req.body
+  let user = await  userInfo.findOne({ email })
+  if (user) {
+   return res.send({message: "This email Id have already registered"})
+    // return res.redirect("/login");
+  }
+
+  user = await userInfo.create({ name, email, password })
+  const token = jwt.sign({ _id: user._id }, "wenhuebfrunsd")
+  res.cookie("token", token, {
+    httpOnly: true, expries: new Date(Date.now() + 60 * 10000)
+  })
+  res.redirect("/Home")
+});
 
 
 app.post('/login', async (req, res) => {
-const {name, email} = req.body
+  const { email, password } = req.body
+  let userExist = await userInfo.findOne({ email })
+  if (!userExist) {
+    return res.redirect("/register")
+  }
+  const isMatchUser = userExist.password === password
 
-  const user = await userInfo.create({name, email})
+  if (!isMatchUser) {
+    return res.render("login", { email, message: "Incorrect User or Password" })
+  }
+  const token = jwt.sign({ _id: userExist._id }, "wenhuebfrunsd")
 
-  const token = jwt.sign({_id: user._id}, "wenhuebfrunsd")
-    
-   res.cookie("token", token, {
-    httpOnly: true, expires: new Date(Date.now()+60*1000)
-   })
-       res.redirect("/Home")
+  res.cookie("token", token, {
+    httpOnly: true, 
+    expires: new Date(Date.now() + 60 * 1000)
+  })
+  res.redirect("/Home")
 
-    });
+});
 
 
-  
-    app.get('/logout',  (req, res) => {
-      res.cookie("token", null, {
-        expires: new Date(Date.now()),
-      })
-          res.redirect("/Home")
-   
-       });
+
+app.get('/logout', (req, res) => {
+  res.cookie("token", null, {
+    expires: new Date(Date.now()),
+  })
+  res.redirect("/Home")
+
+});
 
 
 
